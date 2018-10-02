4d064ca1a7028c9467011a9ec91af88dae6d9bbb
AlleyTheRat <a@uffz.net>
2018-10-02 20:25:14 +0200

    send error code if sth went wrong

--- plugins/kba.js
+++ plugins/kba.js
@@ -30,6 +30,7 @@ function preparation(req, res, endpoint) {
         })
         .catch(function (error) {
             console.log(error);
+            res.status(500).send('Something broke!');
         })
 }
 
